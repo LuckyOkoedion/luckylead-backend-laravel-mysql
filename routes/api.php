3207ba3123e249e1aba2dedfdca432cce3d0fde7
Lucky Okoedion <luckyokoedion2000@gmail.com>
2021-02-06 07:07:33 +0100

    testing the db with returned users api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function () {
+    return User::all();
 });
